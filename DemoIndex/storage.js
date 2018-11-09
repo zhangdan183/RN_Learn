92705452debbc764fe83d3043164c02be064df9e
47217805@qq.com <a9022952>
2018-11-09 17:39:13 +0800

    storage增加文字说明

--- DemoIndex/storage.js
+++ DemoIndex/storage.js
@@ -1,6 +1,14 @@
 import {AsyncStorage} from 'react-native';
 
 class Storage {
+
+    /***
+     * 将一个参数保存到本地存储中
+     * @param key 键
+     * @param value 值
+     * @param callback
+     * @returns {Promise<void>}
+     */
     static putStorage = async (key, value, callback) => {
         try {
             await AsyncStorage.setItem(key, value);
@@ -9,7 +17,11 @@ class Storage {
             callback({states: 'false', msg: ''});
         }
     }
-
+    /***
+     * 根据某一个键来取出本地保存的一个数值
+     * @param key
+     * @returns {Promise<null>}
+     */
     static getStorage = async (key) => {
         try {
             let value = await AsyncStorage.getItem(key);
@@ -24,6 +36,12 @@ class Storage {
 
     }
 
+    /***
+     * 根据一个键值来删除本地存储中的一个参数
+     * @param key 需要删除的某一个key值
+     * @param callback
+     * @returns {Promise<void>}
+     */
     static rmStorage = async (key, callback) => {
         try {
             await AsyncStorage.removeItem(key,function (errs) {
@@ -37,7 +55,11 @@ class Storage {
             callback({states: 'false', msg: ''});
         }
     }
-
+    /***
+     * 清理本地所有储存记录
+     * @param callback
+     * @returns {Promise<void>}
+     */
     static clearStorage = async (callback) => {
         try {
             await AsyncStorage.clear(function (errs) {
@@ -52,10 +74,12 @@ class Storage {
         }
     }
 
-    /*
-    * 下面是批量操作函数
-    * */
-    static setAllStorage = async (keys) => {
+    /***
+     * 将需要保存的数据批量保存到本地储存
+     * @param keys [["1","1"],["2","2"]]
+     * @returns {Promise<void>}
+     */
+    static setAllStorage = async (keys, callback) => {
         try {
             await AsyncStorage.multiSet(keys, function (errs) {
                 if (!errs){
@@ -68,7 +92,11 @@ class Storage {
             callback({states: 'false', msg: errs});
         }
     }
-
+    /**
+     * 获取数据
+     * @param ['key1', 'key2', ...]
+     * @returns {Promise} [["1","1"],["2","2"]]
+     */
     static getAllStorage = async (keys) => {
         try {
             let values = await AsyncStorage.multiGet(keys);
@@ -81,6 +109,37 @@ class Storage {
             return null;
         }
     }
+
+    /***
+     * 批量移除若干个键所对应的值
+     * @param keys ["k1", "k2"]
+     * @returns {Promise<void>}
+     */
+    static rmAllStorage = async (keys, callback) => {
+        try {
+            await AsyncStorage.multiRemove(keys, (errs) => {
+                if (!errs){
+                    callback({state: 'true', msg: ''});
+                } else {
+                    callback({states: 'false', msg: errs});
+                }
+            });
+        } catch (e) {
+            callback({state: 'true', msg: ''});
+        }
+    }
+
+    /***
+     * 暂时没有用，不打算总结这个先
+     * @param keysone
+     * @param keystwo
+     * @param callback
+     * @returns {Promise<void>}
+     */
+    static mergeAllStorage = async (keysone, keystwo, callback) => {
+
+    }
+
 }
 
 export default Storage;
